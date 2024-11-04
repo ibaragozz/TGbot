ee6cfed8cf7ed111e127a73da1850b6c7e351938
ibaragozz <baragozz1@gmail.com>
2024-11-04 14:02:10 +0300

    1s1tlook

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f"Приветствую, {message.from_user.full_name}. Я - бот")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
