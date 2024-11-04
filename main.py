eeeb73715c4f4298a54296b833633da8268663bf
ibaragozz <baragozz1@gmail.com>
2024-11-04 17:54:39 +0300

    lesson2 done

--- main.py
+++ main.py
@@ -5,17 +5,54 @@ from aiogram.types import Message, FSInputFile
 from config import TOKEN, WEATHER_API
 import random
 import requests
+from gtts import gTTS
+import os
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
+@dp.message(Command('doc'))
+async def doc(message: Message):
+    doc = FSInputFile('TG02.pdf')
+    await bot.send_document(message.chat.id, doc)
+
+
+@dp.message(Command('voice'))
+async def voice(message: Message):
+    voice = FSInputFile('sample.ogg')
+    # await bot.send_voice(message.chat.id, voice)
+    message = await message.answer_voice(voice)
+
 @dp.message(Command('video'))
 async def video(message: Message):
-    await message.answer()
+    await bot.send_chat_action(message.chat.id, 'upload_video')
+    video = FSInputFile('jumper.mp4')
+    # await message.answer_video(video)
+    await bot.send_video(message.chat.id, video)
 
 @dp.message(Command('audio'))
 async def audio(message: Message):
-    await message.answer()
+    audio = FSInputFile('fanfars.mp3')
+    await bot.send_audio(message.chat.id, audio)
+
+@dp.message(Command('training'))
+async def training(message: Message):
+    training_list = [
+        "Тренировка 1: \n 1. Скручивания: 3 подхода по 15 повторений \n 2. Велосипед: 3 подхода по 20 повторений (каждая сторона) \n 3. Планка: 3 подхода по 30 секунд",
+        "Тренировка 2: \n 1. Подъемы ног: 3 подхода по 15 повторений \n 2. Русский твист: 3 подхода по 20 повторений (каждая сторона) \n 3. Планка с поднятой ногой: 3 подхода по 20 секунд (каждая нога)",
+        "Тренировка 3: \n 1. Скручивания с поднятыми ногами: 3 подхода по 15 повторений \n 2. Горизонтальные ножницы: 3 подхода по 20 повторений \n 3. Боковая планка: 3 подхода по 20 секунд (каждая сторона)"
+    ]
+    rand_tr = random.choice(training_list)
+    await message.answer(f'Это ваша мини тренировка {rand_tr}')
+    await bot.send_chat_action(message.chat.id, 'upload_voice')
+    tts = gTTS(text = rand_tr, lang = 'ru')
+    tts.save('training.ogg')
+    audio = FSInputFile('training.ogg')
+    await bot.send_voice(message.chat.id, audio)
+    os.remove('training.ogg')
+
+
 
 @dp.message(F.photo)
 async def aussie(message: Message):
