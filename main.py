8bd51868dc94fc3d8a700bb7b83dc062d543d9f4
ibaragozz <baragozz1@gmail.com>
2024-11-04 14:07:17 +0300

    add help

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+@dp.message(Command('help'))
+
 
 @dp.message(CommandStart())
 async def start(message: Message):
