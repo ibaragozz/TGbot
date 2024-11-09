9956c43a235914073ed241842b6e3dfa1cd19f39
ibaragozz <baragozz1@gmail.com>
2024-11-09 17:55:26 +0300

    Update new.py

--- new.py
+++ new.py
@@ -29,12 +29,43 @@ def init_db():
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    age INTEGER,
-    city TEXT)
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    city TEXT NOT NULL)
     """)
     conn.commit()
     conn.close()
+
+init_db()
+
+@dp.message(CommandStart())
+async def start(message: Message, state: FSMContext):
+    await message.answer("Привет!\nКак тебя зовут?")
+    await state.set_state(Form.name.state)
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Сколько тебе лет?")
+    await state.set_state(Form.age.state)
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("В каком городе живешь?")
+    await state.set_state(Form.city.state)
+
+@dp.message(Form.city)
+async def city(message: Message, state: FSMContext):
+    await state.update_data(city=message.text)
+    user_data = await state.get_data()
+
+conn=sqlite3.connect('user_data.db')
+cur=conn.cursor()
+cur.execute("""
+    INSERT INTO users (name, age, city) VALUES (?, ?, ?)""", (user_data['name'], user_data['age'], user_data['city']))
+conn.commit()
+conn.close()
 
 
 
