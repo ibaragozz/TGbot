9e6995a956ad3835cb7cc50186ecc1439f72caab
ibaragozz <baragozz1@gmail.com>
2024-11-09 17:35:43 +0300

    Update new.py

--- new.py
+++ new.py
@@ -20,4 +20,35 @@ from aiogram import Bot, Dispatcher, F, types
 from aiogram.types import ContentType
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, FSInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
 from config import TOKEN
+import aiohttp
+import logging
+import sqlite3
+
+
+
+vbot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
